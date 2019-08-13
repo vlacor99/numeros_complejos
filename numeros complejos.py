ab1d4abddf5a5dd4e565ac2b96a765ca170a1567
vlacor99 <54039061+vlacor99@users.noreply.github.com>
2019-08-13 14:23:33 -0500

    Update numeros complejos.py

--- numeros complejos.py
+++ numeros complejos.py
@@ -20,15 +20,27 @@ def producto(a,b,c,d):
     pr = pp-pq
     pi = px+pz
     if pi>0: resp = "{}+{}i".format(pr,pi)
-    else: resp = "{}-{}i".format(pr,pi)
+    else: resp = "{}{}i".format(pr,pi)
     return resp
-def separar(q):
+def division(a,b,c,d):
+    pp = a*c
+    px = a*-d
+    pz = b*c
+    pq = b*-d
+    pa = pp-pq
+    pb = px+pz
+    pc = (c*c)-(d*-d)
+    if pb/pc>0: resp = "{}+{}i".format(pa/pc,pb/pc)
+    else: resp = "{}{}i".format(pa/pc,pb/pc)
+    return resp
+    
+def separar(q,w):
     s = q[0]
     band = True
     pr,o,pi = "","",""
     cont = 0
     while band:
-        if s not in ["+","-","*","/"] :
+        if s not in ["+","-","*","/"] or cont == 0 :
             pr+=s
             s = q[cont+1]
             cont+=1
@@ -41,9 +53,11 @@ def separar(q):
 def main():
     z = [x for x in stdin.readline()]
     w = [y for y in stdin.readline()]
-    prz,piz = separar(z)
-    prw,piw = separar(w)
-    print("sumar",sumar(prz,piz,prw,piw))
-    print("restar",restar(prz,piz,prw,piw))
-    print("producto",producto(prz,piz,prw,piw))
+    prz,piz = separar(z,0)
+    prw,piw = separar(w,0)
+    print("sumar = ",sumar(prz,piz,prw,piw))
+    print("restar = ",restar(prz,piz,prw,piw))
+    print("producto = ",producto(prz,piz,prw,piw))
+    print("division = ",division(prz,piz,prw,piw))
+    
 main()
